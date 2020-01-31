d50ad9fdbb8507bb23e1022374b799d7f40e2208
ShimaCoding <shimacoding@github.com>
2020-01-31 18:49:28 -0300

    pong test
    pong test

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public ball ball;
+    public paddle paddle;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topright;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0,0));
+        topright = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
+        Instantiate(ball);
+
+        paddle paddle1 = Instantiate(paddle) as paddle;
+        paddle paddle2 = Instantiate(paddle) as paddle;
+
+        paddle1.Init(false);
+        paddle2.Init(true);
+        
+
+
+    }
+
+    
+}
---
+++ Assets/Scenes/ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public float speed;
+    float radius;
+    Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+        radius = transform.localScale.x / 2;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        //rebote izquierda derecha
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0)
+        {
+            direction.y = -direction.y;
+        }
+        if (transform.position.y > GameManager.topright.y - radius && direction.y > 0)
+        {
+            direction.y = -direction.y;
+        }
+
+
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.y < 0)
+        {
+            Debug.Log("El jugador de la derecha gana");
+            Time.timeScale = 0;
+            enabled = false;
+
+        }
+        if (transform.position.x > GameManager.topright.x - radius && direction.y > 0)
+        {
+            Debug.Log("El jugador de la izquierda gana");
+            Time.timeScale = 0;
+            enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Paddle")
+        {
+            bool isRight = other.GetComponent<paddle>().isRight;
+
+            if (isRight == true && direction.x > 0)
+            {
+                direction.x = -direction.x;
+            }
+            if (isRight == false && direction.x < 0)
+            {
+                direction.x = -direction.x;
+            }
+        }
+    }
+}
